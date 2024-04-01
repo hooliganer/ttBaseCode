34d17f79355a07f21dcdb5529cbb7e0cb629c3d2
tantao <taoerist@gmail.com>
2024-04-01 18:38:33 +0800

    链表转数组

--- ttBaseCode/Classes/BaseFunction.swift
+++ ttBaseCode/Classes/BaseFunction.swift
@@ -15,6 +15,20 @@ public class MyLinkListNode<Element> {
         self.val = val
         self.next = next
     }
+    
+    public func toArray() -> [Element] {
+        func getArray(_ array: [Element] = [], node: MyLinkListNode?) -> [Element] {
+            var array = array
+            if let element = node?.val {
+                array.append(element)
+            }
+            if let node = node?.next {
+                return getArray(array, node: node)
+            }
+            return array
+        }
+        return getArray(node: self)
+    }
 }
 
 public extension Array {
---
+++ ttBaseCode/Classes/UIExtenion.swift
@@ -0,0 +1,11 @@
+//
+//  UIExtenion.swift
+//  ttBaseCode_Example
+//
+//  Created by 谭滔 on 2024-04-01.
+//  Copyright © 2024 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+
