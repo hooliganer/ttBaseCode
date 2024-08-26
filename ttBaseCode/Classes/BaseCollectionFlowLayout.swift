43feca8801ff2aac3d6cc934d5421c40a47142ad
tantao <taoerist@gmail.com>
2024-08-26 17:57:07 +0800

    Swift版本适配

--- ttBaseCode/Classes/BaseCollectionFlowLayout.swift
+++ ttBaseCode/Classes/BaseCollectionFlowLayout.swift
@@ -13,20 +13,20 @@ public class BaseCollectionLayout: UICollectionViewFlowLayout {
         case center
         case right
     }
-
+    
     /// 两个Cell之间的距离
     public var betweenOfCell: CGFloat {
         didSet {
             minimumInteritemSpacing = betweenOfCell
         }
     }
-
+    
     /// cell对齐方式
     public var cellType: AlignType = .center
     
     /// 在居中对齐的时候需要知道这行所有cell的宽度总和
     var sumCellWidth: CGFloat = 0.0
-
+    
     override init() {
         betweenOfCell = 5.0
         super.init()
@@ -34,18 +34,18 @@ public class BaseCollectionLayout: UICollectionViewFlowLayout {
         minimumLineSpacing = 5
         sectionInset = .init(top: 5, left: 5, bottom: 5, right: 5)
     }
-
+    
     public convenience init(_ cellType: AlignType) {
         self.init()
         self.cellType = cellType
     }
-
+    
     public convenience init(_ cellType: AlignType, _ betweenOfCell: CGFloat) {
         self.init()
         self.cellType = cellType
         self.betweenOfCell = betweenOfCell
     }
-
+    
     public override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
         let superLayout: [UICollectionViewLayoutAttributes] = super.layoutAttributesForElements(in: rect) ?? [UICollectionViewLayoutAttributes]()
         guard let layoutAttributes = NSArray(array: superLayout, copyItems: true) as? [UICollectionViewLayoutAttributes] else {
@@ -55,20 +55,29 @@ public class BaseCollectionLayout: UICollectionViewFlowLayout {
         for (index, currentAttr) in layoutAttributes.enumerated() {
             let previousAttr = index == 0 ? nil : layoutAttributes[index - 1]
             let nextAttr = index + 1 == layoutAttributes.count ?
-                nil : layoutAttributes[index + 1]
-
+            nil : layoutAttributes[index + 1]
+            
             layoutArray.append(currentAttr)
             sumCellWidth += currentAttr.frame.size.width
-
+            
             let previousY: CGFloat = previousAttr == nil ? 0 : previousAttr!.frame.maxY
             let currentY: CGFloat = currentAttr.frame.maxY
             let nextY: CGFloat = nextAttr == nil ? 0 : nextAttr!.frame.maxY
-
+            
             if currentY != previousY, currentY != nextY {
-                if currentAttr.representedElementKind == UICollectionElementKindSectionHeader {
+                let headerKind: String
+                let footerKind: String
+#if swift(>=4.2)
+                headerKind = UICollectionView.elementKindSectionHeader
+                footerKind = UICollectionView.elementKindSectionFooter
+#else
+                headerKind = UICollectionElementKindSectionHeader
+                footerKind = UICollectionElementKindSectionFooter
+#endif
+                if currentAttr.representedElementKind == headerKind {
                     layoutArray.removeAll()
                     sumCellWidth = 0.0
-                } else if currentAttr.representedElementKind == UICollectionElementKindSectionFooter {
+                } else if currentAttr.representedElementKind == footerKind {
                     layoutArray.removeAll()
                     sumCellWidth = 0.0
                 } else {
@@ -84,7 +93,7 @@ public class BaseCollectionLayout: UICollectionViewFlowLayout {
         }
         return layoutAttributes
     }
-
+    
     /// 调整Cell的Frame
     ///
     /// - Parameter layoutAttributes: layoutAttribute 数组
@@ -120,7 +129,7 @@ public class BaseCollectionLayout: UICollectionViewFlowLayout {
             }
         }
     }
-
+    
     @available(*, unavailable)
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
